a7dc5edba3e0ea670108d94dd866b635b129eba8
Matthew Wheaton <mwheat5487@gmail.com>
2022-03-31 15:45:51 -0400

    turn into load function, store lines in array

--- wfl.pl
+++ wfl.pl
@@ -1,10 +1,28 @@
 #!/usr/bin/perl
 
-$csv_file = "Mobile_Food_Facility_Permit.csv";
 
+# the csv file might make more sense as a command line argument, especially if it changes, or different sources are used
+# it might also be replaced by data on the web retrieved with a get (do we still use cgi-bin?)
+
+$csv_file = "Mobile_Food_Facility_Permit.csv";
 open(CSV, "<$csv_file") or die "cannot open game file $csv_file\n";
 
-foreach $csvline (<CSV>)
+# functional decomposition for a small script can be overkill, but we should make the effort with the idea that
+# we want the code to be modular enough to make changes easily, provide something easier to come back to, or for others to work on
+
+
+sub load_csv()
   {
-    print $csvline;
+
+    @csvline_array = (<CSV>);
+    foreach $csvline (@csvline_array)
+      {
+	print $csvline;
+      }
   }
+##############################
+# main script
+##############################
+
+
+load_csv();
